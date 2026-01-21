436d0a65d03437c1ef27fb09288fd7ae878a6f2b
Elian Muñoz <emunozsalas@icloud.com>
2026-01-21 15:45:59 -0300

    feat: update ticket permissions

--- tickets/permissions.py
+++ tickets/permissions.py
@@ -1,8 +1,36 @@
 from rest_framework.permissions import BasePermission
 
-class IsOwnerOrAdmin(BasePermission):
+class CanCreateTicket(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+
+class CanViewTicket(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (
-            request.user.is_admin or
-            obj.created_by == request.user
-        )
+        user = request.user
+
+        if user.is_admin_role:
+            return True
+
+        if user.is_resolver:
+            return obj.assigned_to == user
+
+        return obj.created_by == user
+
+
+class CanAssignTicket(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_admin_role
+
+
+class CanChangeStatus(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+        if user.is_admin_role:
+            return True
+
+        if user.is_resolver:
+            return obj.assigned_to == user
+
+        return False
