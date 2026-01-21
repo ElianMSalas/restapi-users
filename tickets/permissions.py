cb00f4fdd9a94eb53f62e0959bc1dbf072f9cdbe
Elian Muñoz <emunozsalas@icloud.com>
2026-01-21 15:13:32 -0300

    feat: add ticket permissions

---
+++ tickets/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_admin or
+            obj.created_by == request.user
+        )
