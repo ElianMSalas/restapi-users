3c71221ba2ef1eb397c6af3ddaa4619674ceb887
Elian Muñoz <emunozsalas@icloud.com>
2026-01-21 15:41:21 -0300

    feat: update user model

--- users/models.py
+++ users/models.py
@@ -2,8 +2,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    ROLE_REQUESTER = 'REQUESTER'
+    ROLE_RESOLVER = 'RESOLVER'
+    ROLE_ADMIN = 'ADMIN'
+
+    ROLE_CHOICES = [
+        (ROLE_REQUESTER, 'Solicitante'),
+        (ROLE_RESOLVER, 'Resolutor'),
+        (ROLE_ADMIN, 'Administrador'),
+    ]
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=ROLE_REQUESTER
+    )
+
     email = models.EmailField(unique=True)
-    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
