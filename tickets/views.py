a3678ab562b0e55e849034b0c00aecc96fd706eb
Elian Muñoz <emunozsalas@icloud.com>
2026-01-21 15:19:05 -0300

    feat: add tickets status update and assign ticket own + comments

--- tickets/views.py
+++ tickets/views.py
@@ -3,6 +3,16 @@ from .models import Ticket
 from .serializers import TicketSerializer
 from .permissions import IsOwnerOrAdmin
 
+#imports endpoints
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Ticket
+from users.permissions import IsAdmin
+
+
+#ticket list
 class TicketListCreateView(generics.ListCreateAPIView):
     serializer_class = TicketSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -16,7 +26,28 @@ class TicketListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
+#detail ticket
 class TicketDetailView(generics.RetrieveAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
+
+#change status
+class TicketStatusUpdateView(APIView):
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+
+    def patch(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        ticket.status = request.data.get('status')
+        ticket.save()
+        return Response({'status': 'updated'})
+
+#assign ticket owner
+class TicketAssignView(APIView):
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+
+    def patch(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        ticket.assigned_to_id = request.data.get('user_id')
+        ticket.save()
+        return Response({'assigned': True})
