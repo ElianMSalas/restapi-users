fdb57fe04f6d9d5449c8c6114a5f1899dbcdc130
Elian Muñoz <emunozsalas@icloud.com>
2026-01-21 15:46:27 -0300

    feat: update tickets views

--- tickets/views.py
+++ tickets/views.py
@@ -1,53 +1,57 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Ticket
 from .serializers import TicketSerializer
-from .permissions import IsOwnerOrAdmin
+from .permissions import (
+    CanCreateTicket,
+    CanViewTicket,
+    CanAssignTicket,
+    CanChangeStatus
+)
 
-#imports endpoints
+#endpoints
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from .models import Ticket
 from users.permissions import IsAdmin
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-
-#ticket list
 class TicketListCreateView(generics.ListCreateAPIView):
     serializer_class = TicketSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, CanCreateTicket]
 
     def get_queryset(self):
         user = self.request.user
-        if user.is_admin:
+
+        if user.is_admin_role:
             return Ticket.objects.all()
+
+        if user.is_resolver:
+            return Ticket.objects.filter(assigned_to=user)
+
         return Ticket.objects.filter(created_by=user)
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
 
-#detail ticket
 class TicketDetailView(generics.RetrieveAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
+    permission_classes = [IsAuthenticated, CanViewTicket]
 
-#change status
-class TicketStatusUpdateView(APIView):
-    permission_classes = [permissions.IsAuthenticated, IsAdmin]
-
-    def patch(self, request, pk):
-        ticket = get_object_or_404(Ticket, pk=pk)
-        ticket.status = request.data.get('status')
-        ticket.save()
-        return Response({'status': 'updated'})
-
-#assign ticket owner
 class TicketAssignView(APIView):
-    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    permission_classes = [IsAuthenticated, CanAssignTicket]
 
     def patch(self, request, pk):
         ticket = get_object_or_404(Ticket, pk=pk)
         ticket.assigned_to_id = request.data.get('user_id')
         ticket.save()
         return Response({'assigned': True})
+    
+class TicketStatusUpdateView(APIView):
+    permission_classes = [IsAuthenticated, CanChangeStatus]
+
+    def patch(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        ticket.status = request.data.get('status')
+        ticket.save()
+        return Response({'status': 'updated'})
