aabb33ff889324d98027c498292948ed9d615942
Elian Muñoz <emunozsalas@icloud.com>
2026-01-21 15:14:03 -0300

    feat: add ticket views

---
+++ tickets/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, permissions
+from .models import Ticket
+from .serializers import TicketSerializer
+from .permissions import IsOwnerOrAdmin
+
+class TicketListCreateView(generics.ListCreateAPIView):
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_admin:
+            return Ticket.objects.all()
+        return Ticket.objects.filter(created_by=user)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
